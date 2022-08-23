5b9e10ce23e87e3bf6f9e50b3e2dd19283606f58
Alexandra Rodriguez-Beuerman <alexandra@classkick.com>
2022-08-23 13:27:51 -0500

    added react router

--- next-trip/src/App.js
+++ next-trip/src/App.js
@@ -1,14 +1,38 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 import RouteSelector from './RouteSelector';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <header className="App-header">
         Next Trip
+        <ul>
+          <li>
+            <Link to='/'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to='/route-selector'>
+              Select a route
+            </Link>
+          </li>
+        </ul>
       </header>
-      <RouteSelector />
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/route-selector' element={<RouteSelector/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
